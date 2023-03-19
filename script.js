9c8e3ec94d87b0777eaa11564a751c258c890d16
vodka-boy01 <70869541+vodka-boy01@users.noreply.github.com>
2023-03-19 22:34:07 +0100

    1

---
+++ script.js
@@ -0,0 +1,13 @@
+const searchInput = document.querySelector('input[type="search"]');
+//solo tipi di input search
+searchInput.addEventListener('focus', function() {
+  if(this.value === '     CERCA') {
+    this.value = '';
+  }
+});
+//testo che scompare nella digitazione v
+searchInput.addEventListener('blur', function() {
+  if(this.value === '') {
+    this.value = '     CERCA';
+  }
+});
