55ee6446cc1069a3a4f6cb05757cebc966334766
vodka-boy01 <70869541+vodka-boy01@users.noreply.github.com>
2023-03-21 00:16:26 +0100

    6

--- script.js
+++ script.js
@@ -5,15 +5,25 @@ searchInput.addEventListener('focus', function() {
     this.value = '';
   }
 });
-//testo che scompare nella digitazione v
+//testo che scompare nella digitazione 
 searchInput.addEventListener('blur', function() {
   if(this.value === '') {
     this.value = 'CERCA';
   }
 });
+/*cancella il campo di ricerca*/
 function clearSearch(){
   document.getElementById("search-input").value = "CERCA";
 }
+/*cambio dinamico dei colori del sito: std su bianco///////////////*/
+const colorChange = document.getElementById('login-icon');
+/*var colorValue = 0;if(colorValue === )*/
+colorChange.addEventListener('click', function(){
+  /*aggiungere tutti i colori per le i cambi dinamici, e aggiungere un bottone al lato sinistro del bottone login per la transizione di colore della pagina*/
+  document.documentElement.style.setProperty('--main--color', 'red');
+});
+
+
 /*
 function clearSearch() {//
   document.getElementById("reset").onclick = function(){
