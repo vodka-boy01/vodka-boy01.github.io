d08a0d222ee4849f5cec81179c9da37ebe701a85
vodka-boy01 <70869541+vodka-boy01@users.noreply.github.com>
2023-03-21 17:27:35 +0100

    6

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
+//testo che scompare nella digitazione 
 const searchInput = document.getElementById('search-input');
-//solo tipi di input search
 searchInput.addEventListener('focus', function() {
   if(this.value === 'CERCA') {
     this.value = '';
   }
 });
-//testo che scompare nella digitazione 
 searchInput.addEventListener('blur', function() {
   if(this.value === '') {
     this.value = 'CERCA';
   }
 });
+/*quando c'è qualcosa scritto in input search rimane in focus*/
+
 /*cancella il campo di ricerca*/
 function clearSearch(){
   document.getElementById("search-input").value = "CERCA";
 }
 /*cambio dinamico dei colori del sito: std su bianco///////////////*/
-const colorChange = document.getElementById('login-icon');
+const colorChange = document.getElementById('circle-icon');
 /*var colorValue = 0;if(colorValue === )*/
 colorChange.addEventListener('click', function(){
   /*aggiungere tutti i colori per le i cambi dinamici, e aggiungere un bottone al lato sinistro del bottone login per la transizione di colore della pagina*/
-  document.documentElement.style.setProperty('--main--color', 'red');
+  document.documentElement.style.setProperty('--main-color', 'red');
 });
 
 
