56bd058ed57a0b965107ae4d11efb9f4f6168047
vodka-boy01 <luigitanzillo2006@gmail.com>
2024-04-01 03:47:33 +0200

    1

--- assets/js/script_loginPage.js
+++ assets/js/script_loginPage.js
@@ -1,4 +1,8 @@
 document.getElementById("form").addEventListener("submit", function(event){
     event.preventDefault();
 
-});
+});//TODO da togliere
+
+function bordoRosso(){
+    document.getElementsByClassName('error_br');
+}
